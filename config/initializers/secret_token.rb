b2875f2282a65223e9fe26516b56ee533b52a318
nick n <nicckk@gmail.com>
2014-02-25 20:07:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+TwitApp::Application.config.secret_key_base = 'c1a07591630af429e3a11d757e6bab22d80bf8f29d964e7841e07a12c19e5f69410c42ccef316314f819d8db080606720c66839644edd91c587cbbfa76ed5a72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitApp::Application.config.session_store :cookie_store, key: '_twit_app_session'
