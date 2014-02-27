82c046027d1a54202c393700b628e4e346ce6cb4
nick n <nicckk@gmail.com>
2014-02-27 09:32:01 +0000

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "Home page" do
-    it "should have content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+describe "StaticPages" 
+
+      it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
-  visit '/static_pages/home'
-  expect(page).to have_title("Home")
-	end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
